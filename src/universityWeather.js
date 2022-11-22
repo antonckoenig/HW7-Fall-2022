9fd3c5be2af0451c40156694d48c5c8ed1cfcf13
Mike <mike.b.perreault@gmail.com>
2022-11-22 01:13:22 -0500

    Added questions 4-8. need some more testing, especially on read/write to file

--- src/universityWeather.js
+++ src/universityWeather.js
@@ -7,7 +7,7 @@ export function fetchUniversityWeather(university) {
   const locations = universities.then(arr => arr.map(school => fetchLongitudeAndLatitude(school))).then(arr =>Promise.all(arr));
   const weather = locations.then(locs => locs.map(location => fetchCurrentWeather(location.lon, location.lat))).then(arr => Promise.all(arr));
   const temps = weather.then(weathers => weathers.map(w => w.temperature_2m.reduce((acc,e) => acc + e,0)/w.temperature_2m.length)).then(arr => Promise.all(arr));
-  const average = temps.then(arr => arr.reduce((acc, e)=> acc+e, 0)/arr.length);
+  const average = temps.then(arr => arr.length ? arr.reduce((acc, e)=> acc+e, 0)/arr.length : 0);
   return Promise.all([temps, universities, average]).then((arr) => arr[1].reduce((acc, e, i) => ({...acc, [e]: arr[0][i] }),{totalAverage:arr[2]}));
 }
 
